76b75b8bd3bf8484c93b5affdb01a1d41bf9f290
Roopansh Bansal <roopansh.bansal@gmail.com>
2016-10-22 11:47:56 +0530

    Added error messages

--- rtl8723be.sh
+++ rtl8723be.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-sudo modprobe -r rtl8723be
+if [ $EUID -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
 echo "Removing rtl8723be module form the kernel"
-echo "Entering /etc/modprobe.d/"
-echo "Changing the rtl8723be settings" 
-echo "options rtl8723be fwlps=N ips=N ant_sel=1" | sudo tee /etc/modprobe.d/rtl8723be.conf
-echo "Adding rtl8723be module back to the kernel"
-sudo modprobe rtl8723be
+modprobe -r rtl8723be
+if [ "$?" = "0" ]; then
+	echo "Entering /etc/modprobe.d/"
+	echo "Changing the rtl8723be settings"
+	echo "options rtl8723be fwlps=N ips=N ant_sel=1" | sudo tee /etc/modprobe.d/rtl8723be.conf
+	if [ "$?" = "0" ]; then
+		echo "Adding rtl8723be module back to the kernel"
+		echo "Return status 0"
+	else
+		echo "There were errors editing the module options"
+		echo "Process returned with some errors"
+		modprobe rtl8723be
+		return 1
+	fi
+else
+	echo "There were errors removing the Module from the kernel"
+	echo "Process returned with some errors"
+	return 1
+fi
+modprobe rtl8723be
